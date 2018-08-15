8b2b314d5cf9d56ad4f8a260e36a6ff32fe2792f
Jonathan Chan <johnkchan252@gmail.com>
2018-08-15 01:51:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join("")
   else
     array[-1] = "and " + array[-1]
-    if array.size = 2
+    if array.size ==2
       array.join(" ")
     else
       array.join(", ")
